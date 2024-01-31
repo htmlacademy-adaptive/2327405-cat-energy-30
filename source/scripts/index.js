e0842fb6e945f28e25b09acfe08730c9d58970dd
Maxim Novikov <mn7795807@gmail.com>
2024-01-31 03:59:24 +0300

    Первая попытка защиты

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,37 +1,37 @@
 /* в этот файл добавляет скрипты*/
+const navigation = document.querySelector('.navigation');
+const mainHeaderButtons = document.querySelectorAll('.main-header__button');
+const burger = document.querySelector('.main-header__item--burger');
+const cross = document.querySelector('.main-header__item--cross');
+
 const sliderToggle = document.querySelector('.slider__toggle');
 const slider = document.querySelector('.slider');
 
-let flag = false;
-sliderToggle.addEventListener('click', () => {
-  if(flag) {
-    slider.classList.toggle('slider--after');
-    slider.classList.toggle('slider--before');
-  } else {
-    flag = true;
-    slider.classList.add('slider--after');
+
+if (slider) {
+  let flag = false;
+  sliderToggle.addEventListener('click', () => {
+    if (flag) {
+      slider.classList.toggle('slider--after');
+      slider.classList.toggle('slider--before');
+    } else {
+      flag = true;
+      slider.classList.add('slider--after');
+    }
+  });
 }
+
+navigation.classList.add('navigation__close');
+burger.classList.add('main-header__item--show')
+
+mainHeaderButtons.forEach((item) => {
+  item.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    navigation.classList.toggle('navigation__open');
+    navigation.classList.toggle('navigation__close');
+    burger.classList.toggle('main-header__item--show');
+    cross.classList.toggle('main-header__item--show');
+  })
 });
-
-const navigation = document.querySelector('.navigation');
-const mainHeaderButton = document.querySelector('.main-header__button');
-const burger = document.querySelector('.main-header__item--open');
-const cross = document.querySelector('.main-header__item--close');
-
-mainHeaderButton.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  navigation.classList.add('navigation__open');
-});
-
-mainHeaderButton.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  burger.classList.add('navigation__close');
-});
-
-mainHeaderButton.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  cross.classList.add('navigation__open');
-});
-
 
 
